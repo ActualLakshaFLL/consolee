e0e724a3b030d5d14b185b0d2160b72fb2f62a13
ActualLakshaFLL <114113686+ActualLakshaFLL@users.noreply.github.com>
2023-05-24 20:48:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Kenya";
+            console.log(first_name);
+            var last_name = "Pattason";
+            console.log(last_name);
+            var full_name =
+            first_name.concat
+            (last_name);
+            console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
+
